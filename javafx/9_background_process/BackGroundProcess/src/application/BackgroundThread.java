eb20e63cb95330ee63fdd35ffc0707e4c34d58b1
Saketh Katari <katari15045@iiitd.ac.in>
2017-12-28 16:02:32 +0530

    Javafx : Adds 9_background_process

---
+++ javafx/9_background_process/BackGroundProcess/src/application/BackgroundThread.java
@@ -0,0 +1,36 @@
+package application;
+
+import javafx.concurrent.Task;
+import javafx.scene.control.Button;
+
+public class BackgroundThread extends Task<Boolean>
+{	
+	private Button button = null;
+	
+	public BackgroundThread(Button button) 
+	{
+		this.button = button;
+	} 
+	
+	@Override
+	public Boolean call()
+	{
+		Integer count = 0;
+		
+		while( count < 999999999 )
+		{
+			count = count + 1;
+		}
+		
+		button.setDisable(false);
+		updateProgress(1.0, 1.0); // https://docs.oracle.com/javafx/2/ui_controls/progress.htm#CHDJJCFD
+		
+		return true;
+	}
+	
+	@Override
+	protected void updateProgress(double workDone, double max) 
+	{
+		super.updateProgress(workDone, max);
+	}
+}
---
+++ javafx/9_background_process/BackGroundProcess/src/application/Main.java
@@ -0,0 +1,99 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.GridPane;
+
+
+public class Main extends Application 
+{
+	private GridPane gridPane= null; 
+	private Scene scene = null;
+	
+	private ProgressIndicator progressIndicator = null;
+	private Button button = null;
+	
+	private BackgroundThread backgroundThread = null;
+	private Thread thread = null;
+	
+	@Override
+	public void start(Stage stage) 
+	{
+		progressIndicator = new ProgressIndicator();
+		progressIndicator.setProgress(-1.0);	// https://docs.oracle.com/javafx/2/ui_controls/progress.htm#CHDJJCFD
+		button = new Button();
+		button.setText("Next step");
+		button.setOnAction( new MyHandler(stage) );
+		button.setDisable(true);
+		
+		gridPane = new GridPane();
+		gridPane.add(progressIndicator, 0, 0);
+		gridPane.add(button, 0, 1);
+		gridPane.setAlignment(Pos.CENTER);
+		GridPane.setMargin(button, new Insets(60, 0, 0, 0));
+		
+		scene = new Scene(gridPane, 400, 200);
+		stage.setScene(scene);
+		stage.show();
+		
+		backgroundThread = new BackgroundThread(button);
+		progressIndicator.progressProperty().bind( backgroundThread.progressProperty() );
+		thread = new Thread(backgroundThread);
+		thread.start();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
+
+class MyHandler implements EventHandler<ActionEvent>
+{
+	private Label label = null;
+	private GridPane gridPane = null;
+	private Scene scene = null;
+	private Stage stage = null;
+	
+	public MyHandler(Stage stage)
+	{
+		this.stage = stage;
+	}
+	
+	@Override
+	public void handle(ActionEvent event)
+	{
+		label = new Label();
+		label.setText("WOW!!!");
+		
+		gridPane = new GridPane();
+		gridPane.add(label, 0, 0);
+		gridPane.setAlignment(Pos.CENTER);
+		
+		scene = new Scene(gridPane, 600, 400);
+		stage.setScene(scene);
+		stage.show();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
