2732dfa06aa065b01e865ebcdf402081a63b43d8
Saketh Katari <katari15045@iiitd.ac.in>
2018-07-15 20:36:43 +0530

    basics: adds 9_sum_of_array_elements_without_loop

---
+++ basics/misc/9_sum_of_array_elements_without_loop/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Main{
+	public static void main(String[] args){
+		ArrayList<Integer> arr = new ArrayList<>();
+		arr.add(1);
+		arr.add(2);
+		arr.add(3);
+		System.out.println(sum(arr, 0));
+
+	private static int sum(ArrayList<Integer> arr, int ind){
+		if(ind == (arr.size()-1)){
+			return arr.get(ind);
+		}
+		return arr.get(ind) + sum(arr, ind+1);
+	}
+}
