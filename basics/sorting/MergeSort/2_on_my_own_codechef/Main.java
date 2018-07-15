0ca77a2c9c2d9954bdc9203bb3edb56342aad690
Saketh Katari <katari15045@iiitd.ac.in>
2018-07-15 15:15:09 +0530

    sorting : updates my own version of merge sort

--- basics/sorting/MergeSort/2_on_my_own_codechef/Main.java
+++ basics/sorting/MergeSort/2_on_my_own_codechef/Main.java
@@ -1,126 +1,58 @@
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Iterator;
 
-public class Main
-{
-	private static BufferedReader bufferedReader;
-	private static BufferedOutputStream bufferedOutputStream;
-	
-	public static void main(String[] args) throws IOException
-	{
-		int length;
-		int currentIndex = 0;
-		ArrayList<Integer> arrayList;
-		
-		bufferedReader = new BufferedReader( new InputStreamReader(System.in) );
-		bufferedOutputStream = new BufferedOutputStream(System.out);
-		
-		length = Integer.valueOf( bufferedReader.readLine() );
-		arrayList = new ArrayList<>(length);
-		
-		while( currentIndex < length )
-		{
-			arrayList.add( Integer.valueOf( bufferedReader.readLine() ) );
-			currentIndex = currentIndex + 1;
-		}
-		
-		mergeSort(arrayList, 0, length-1);
-		printArray(arrayList);
-		
-		bufferedReader.close();
-		bufferedOutputStream.flush();
-		bufferedOutputStream.close();
+public class Main{
+	public static void main(String[] args){
+		ArrayList<Integer> arr = new ArrayList<>();
+		arr.add(1);
+		arr.add(1);
+		arr.add(2);
+		arr.add(3);
+		arr.add(4);
+		System.out.println(arr);
+		mergeSort(arr, 0, arr.size()-1);
+		System.out.println(arr);
 	}
-	
-	private static void printArray(ArrayList<Integer> array) throws IOException
-	{
-		Iterator<Integer> iterator = array.iterator();
-		
-		while( iterator.hasNext() )
-		{
-			String tempStr = iterator.next().toString() + "\n";
-			bufferedOutputStream.write( tempStr.getBytes() );
+
+	private static void mergeSort(ArrayList<Integer> arr, int startIndex, int endIndex){
+		if(startIndex == endIndex){
+			return;
 		}
+		int midIndex = (startIndex+endIndex)/2;
+		mergeSort(arr, startIndex, midIndex);
+		mergeSort(arr, midIndex+1, endIndex);
+		merge(arr, startIndex, midIndex, endIndex);
 	}
-	
-	private static void mergeSort(ArrayList<Integer> array, int startIndex, int endIndex) throws IOException
-	{
-		int midIndex = (startIndex + endIndex) / 2;
-		
-		if( midIndex > startIndex )
-		{
-			mergeSort(array, startIndex, midIndex);
-		}
-		
-		if( endIndex > (midIndex+1) )
-		{
-			mergeSort(array, midIndex+1, endIndex);
-		}
-		
-		merge(array, startIndex, endIndex);
-	}
-	
-	private static void merge(ArrayList<Integer> array, int startIndex, int endIndex) throws IOException
-	{
-		ArrayList<Integer> mergedArray = new ArrayList<>( (endIndex-startIndex)+1 );
-		int midIndex = (startIndex + endIndex) / 2;
-		int indexPointerOne = startIndex;
-		int indexPointerTwo = midIndex + 1;
-		int currentIndex = 0;
-		
-		while( indexPointerOne <= midIndex && indexPointerTwo <= endIndex )
-		{
-			if( array.get(indexPointerOne) <= array.get(indexPointerTwo) )
-			{
-				mergedArray.add( array.get(indexPointerOne) );
-				indexPointerOne = indexPointerOne + 1;
-			}
-			
-			else
-			{
-				mergedArray.add( array.get(indexPointerTwo) );
-				indexPointerTwo = indexPointerTwo + 1;
+
+	private static void merge(ArrayList<Integer> inpArray, int startIndex, int midIndex, int endIndex){
+		ArrayList<Integer> mergedArr = new ArrayList<>();
+		int p1, p2, val1, val2;
+		p1 = startIndex;
+		p2 = midIndex+1;
+		while( p1 <= midIndex && p2 <= endIndex){
+			val1 = inpArray.get(p1);
+			val2 = inpArray.get(p2);
+			if(val1 <= val2){
+				mergedArr.add(val1);
+				p1 = p1+1;
+			}else{
+				mergedArr.add(val2);
+				p2 = p2+1;
 			}
 		}
-		
-		while( indexPointerOne <= midIndex )
-		{
-			mergedArray.add( array.get(indexPointerOne) );
-			indexPointerOne = indexPointerOne + 1;
+		while(p1 <= midIndex){
+			mergedArr.add(inpArray.get(p1));
+			p1 = p1+1;
 		}
-		
-		while( indexPointerTwo <= endIndex )
-		{
-			mergedArray.add( array.get(indexPointerTwo) );
-			indexPointerTwo = indexPointerTwo + 1;
+		while(p2 <= endIndex){
+			mergedArr.add(inpArray.get(p2));
+			p2 = p2+1;
 		}
-		
-		//String tempStr = "Actual Array -> " + array.toString() + "\n[" + startIndex + ", " + endIndex + "]\nMerged Array -> " + mergedArray.toString() + "\n";
-		//System.out.println(tempStr);
-		
-		while( currentIndex < mergedArray.size() )
-		{
-			array.set( startIndex+currentIndex, mergedArray.get(currentIndex) );
-			currentIndex = currentIndex + 1;
+		p1 = startIndex;
+		p2 = 0;
+		while(p1 <= endIndex){
+			inpArray.set(p1, mergedArr.get(p2));
+			p1 = p1+1;
+			p2 = p2+1;
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
